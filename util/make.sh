74fc05371cf20b42fbdaedcd08838cc058e5bdf1
Adam D <adam820@gmail.com>
2014-11-17 05:05:55 -0500

    Syntax changes.

--- util/make.sh
+++ util/make.sh
@@ -2,7 +2,7 @@
 # Make.sh
 
 # Fail if no source directory has been passed.
-if [ ! -d $1 ]; then
+if [ ! -d "$1" ] || [ "$1" = "" ]; then
 	echo Cannot run this script from the command line.
 	exit 1
 fi
@@ -11,35 +11,35 @@ WORKDIR=obj
 SRCDIR=$1
 
 # Delete existing work directory and create a new work directory
-	rm -rf $WORKDIR #2&>/dev/null
+	rm -rf $WORKDIR 2>/dev/null
 	mkdir $WORKDIR
 
 # Copy data/code from the source directory to the work directory.
 echo -n Copying make files...
-cp $SRCDIR/make.txt $WORKDIR/make.txt #2&>/dev/null
+cp $SRCDIR/make.txt $WORKDIR/make.txt 2>/dev/null
 echo -ne "\n" >> $WORKDIR/make.txt
 echo [done]
 
 if [ -d $SRCDIR/data ]; then
 	echo -n Copying Data files...
 	mkdir $WORKDIR/data
-	cp -R $SRCDIR/data/* $WORKDIR/data/ #2&>/dev/null
+	cp -R $SRCDIR/data/* $WORKDIR/data/ 2>/dev/null
 	echo [done]
 fi
 
 echo -n Copying PRG files... 
 mkdir $WORKDIR/PRG
-cp $SRCDIR/PRG/* $WORKDIR/PRG/ #2&>/dev/null
+cp $SRCDIR/PRG/* $WORKDIR/PRG/ 2>/dev/null
 echo [done]
 
 # util\if6502 $SRCDIR/code $WORKDIR/code -all
 echo -n Copying code files...
 mkdir $WORKDIR/code
-cp $SRCDIR/code/* $WORKDIR/code/ #2&>/dev/null
+cp $SRCDIR/code/* $WORKDIR/code/ 2>/dev/null
 echo [done]
 
 # Create make.asm file to combine all assembled PRGs into the final ROM.
-rm -f $WORKDIR/make.asm #2&>/dev/null
+rm -f $WORKDIR/make.asm 2>/dev/null
 touch $WORKDIR/make.asm
 echo ".outfile \"bin/${SRCDIR}.nes\"" >> $WORKDIR/make.asm
 echo ".include \"code/header.asm\"" >> $WORKDIR/make.asm
@@ -48,7 +48,7 @@ echo ".include \"code/header.asm\"" >> $WORKDIR/make.asm
 # Compile each of the banks in make.txt, and add each to the make.asm file.
 while IFS="," read f1 f2 f3 f4 f5;
 do
-	echo Assembling $f2:
+	echo -n "Assembling ${f2}: "
 	./util/ophis21/bin/ophis -o "${WORKDIR}/${f3}.bin" "${WORKDIR}/PRG/${f3}.asm"
 	echo ".incbin \"${f3}.bin\"" >> $WORKDIR/make.asm
 done < $WORKDIR/make.txt
@@ -71,8 +71,7 @@ done < $WORKDIR/make.txt
 rm -rf bin 2&>/dev/null && mkdir bin
 
 # Combine the banks
-echo -n Combining banks into ROM...
+echo -n "Combining banks into ROM: "
 ./util/ophis21/bin/ophis obj/make.asm
-echo [done]
 
 exit 0
